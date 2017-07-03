389421420b8ac40115c9bb45e1207a36195afa66
Lucariofan843 <Darklucario1553@gmail.com>
2017-07-03 15:00:51 -0400

    Part II temp

--- src/App.js
+++ src/App.js
@@ -2,33 +2,62 @@ import React,  { Component } from 'react'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css'
 
+class Staff1 extends Component {
+ render () {
+     return (
+
+         <div className="Name1">
+             <h1>Charlie Im</h1>
+             <div className="Card-Pic-1">
+             </div>
+         </div>
+
+
+)
+ }
+
+}
+
+
 class About extends Component {
  render() {
      return (
-
+     <Router>
          <div className="Title2">
-             <h1>About.</h1>
+             <h1>About</h1>
+
          <div className="History">
+             <body>
              <p>Change is a non-profit charitable organization that has provided excellent person-centered programs and services to individuals with disabilities for over 50 years.
 
                  Change, Inc. was founded in 1954 as a Children's Program by a group of parents who realized a program was needed to care for the needs of  their children with developmental disabilities.
 
                  Carroll Haven Center was incorporated in 1969 and increased their services to include adults as well as children.
 
-                 In 1992 Change adopted its new name (Carroll Haven Achieving New Growth Experiences). What first started as a small program to address only a few children in Carroll County has now grown to servicing nearly 200 individuals in five counties.  Change partners with families, caregivers, and advocates to provide excellent opportunities to all those we serve. We embrace evidence based, customized approaches to continuously improve person-centered outcomes.</p> </div>
-        <div className="Card-Pic-1">
-            <picture>
-            <source type="image/jpg" src="Charlie.jpg" />
-            </picture>
-         </div>
-            <div className="Name1">
-                <h1>Charlie Im</h1>
-            </div>
+                 In 1992 Change adopted its new name (Carroll Haven Achieving New Growth Experiences). What first started as a small program to address only a few children in Carroll County has now grown to servicing nearly 200 individuals in five counties.  Change partners with families, caregivers, and advocates to provide excellent opportunities to all those we serve. We embrace evidence based, customized approaches to continuously improve person-centered outcomes.</p> </body> </div>
+       <div className="Staff">
+        <div>
+            <h1>Staff Members</h1>
+            <div>
+            <ul>
+                <li>
+                    <Link to="/About/Staff/1"> Charlie Im</Link>
+                     </li>
+                <li>
+                    <Link to="About/Staff/2">Teresa O'Brien</Link> </li>
+                <li>
+                  <Link to="About/Staff/3">Court Dudek</Link>
+                </li>
+            </ul>
+        </div>
+        </div>
 
 
          </div>
 
-
+         </div>
+         <Route path="About/Staff/1" component={Staff1}/>
+     </Router>
      );
  }
 }
