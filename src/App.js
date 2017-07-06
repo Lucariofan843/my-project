616821affb35ab573b1a3473126955ed0eeddd4b
Lucariofan <Darklucario1553@gmail.com>
2017-07-06 14:50:42 -0400

    Part II Temp Events Route

--- src/App.js
+++ src/App.js
@@ -12,12 +12,10 @@ class About extends Component {
              <h1>About</h1>
          </div>
          <div className="History">
-
              <p>Change, Inc. was founded in 1954 as a Children's Program by a group of parents who realized a program was needed to care for the needs of  their children with developmental disabilities.
+                Carroll Haven Center was incorporated in 1969 and increased their services to include adults as well as children.
+                In 1992 Change adopted its new name (Carroll Haven Achieving New Growth Experiences). What first started as a small program to address only a few children in Carroll County has now grown to servicing nearly 200 individuals in five counties.  Change partners with families, caregivers, and advocates to provide excellent opportunities to all those we serve. We embrace evidence based, customized approaches to continuously improve person-centered outcomes.</p> </div>
 
-                 Carroll Haven Center was incorporated in 1969 and increased their services to include adults as well as children.
-
-                 In 1992 Change adopted its new name (Carroll Haven Achieving New Growth Experiences). What first started as a small program to address only a few children in Carroll County has now grown to servicing nearly 200 individuals in five counties.  Change partners with families, caregivers, and advocates to provide excellent opportunities to all those we serve. We embrace evidence based, customized approaches to continuously improve person-centered outcomes.</p> </div>
        <div className="staffMembers">
             <h1>Staff Members</h1>
         </div>
@@ -42,8 +40,8 @@ class About extends Component {
                     <div className="jobTitle">DSP</div>
                 </div>
 
-             </div>
-             </div>
+            </div>
+         </div>
      );
  }
 }
@@ -61,7 +59,8 @@ class Home extends Component {
          <div className="Welcome"> Change is a non-profit charitable organization that has provided excellent person-centered programs and services to individuals with disabilities for over 50 years.</div>
      </div>
      <div className="Homeimages">
-         <div className="Homeimage1"/> <div className="Homeimage2"/>
+         <div className="Homeimage1"/>
+         <div className="Homeimage2"/>
 
          </div>
          <div className="Costs">Contact us for more details.</div>
@@ -72,39 +71,72 @@ class Home extends Component {
  );
  }
 
+}
+
+class Events extends Component {
+
+
+       render () {
+
+           let  EventsList = {
+               "name": "Change",
+               "events": [
+                   {
+                      "type": "golf",
+                       "date": "June 8th",
+                       "description": "proceeds from this event benefit the individuals and families of Change, Inc."
+                   }
+
+
+        ]
+               };
+
+
+           const array = EventsList.map(function(obj){
+               const rObj = {};
+               rObj[obj.type] = obj.date;
+               rObj[obj.type] = obj.description;
+               return rObj;
+           })
 
 
 }
 
+
+       }
+
+
+
 class App extends Component {
   render() {
     return (
-      <Router><div className="App-hero">
-          <div className="App-header">
-              <div className="header h1">
+      <Router>
+        <div className="App-hero">
+            <div className="App-header">
+                <div className="header h1">
 
-            <header>
-                <h1>Change Inc.</h1>
-            </header>
+                    <header>
+                        <h1>Change Inc.</h1>
+                    </header>
 
-              </div>
+                </div>
             </div>
-          <div className="nav">
+            <div className="nav">
               <div className="nav Link">
+
                 <nav>
                     <padding><Link to="/">Home</Link></padding> |
                     <padding><Link to="/About">About</Link></padding> |
-                    <padding href="/Events">Events</padding>
+                    <padding><Link to="/Events">Events</Link></padding>
                 </nav>
+
               </div>
-          </div>
+            </div>
 
-                 <Route path="/" exact component={Home} />
-                 <Route path="/About" exact component={About} />
-
-
-
-      </div>
+            <Route path="/" exact component={Home} />
+            <Route path="/About" exact component={About} />
+            <Route path="/Events" exact component={Events} />
+        </div>
       </Router>
     );
 
