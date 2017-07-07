12d9852bb5d1b1b2a910818a7b0b49f7f2d463dd
Lucariofan <Darklucario1553@gmail.com>
2017-07-06 22:48:49 -0400

    Part II Final Commit

--- src/App.js
+++ src/App.js
@@ -75,36 +75,42 @@ class Home extends Component {
 
 class Events extends Component {
 
-
        render () {
+           return (
+       <div>
+           <div className="eventText"><h1>Change Inc's Events</h1></div>
+           <div className="eventsList">
 
-           let  EventsList = {
-               "name": "Change",
-               "events": [
-                   {
-                      "type": "golf",
-                       "date": "June 8th",
-                       "description": "proceeds from this event benefit the individuals and families of Change, Inc."
-                   }
+               <div className="event1">
 
+                       <div className="title">Golf Tournament</div>
+                       <div className="eventimage"/>
+                       <div className="date">June 8th</div>
+                       <div className="description">proceeds from this event benefit the individuals and families of Change, Inc.          </div>
+               </div>
 
-        ]
-               };
+               <div className="event2">
 
-
-           const array = EventsList.map(function(obj){
-               const rObj = {};
-               rObj[obj.type] = obj.date;
-               rObj[obj.type] = obj.description;
-               return rObj;
-           })
-
-
-}
+               <div className="title">Bingo</div>
+               <div className="event2image"/>
+               <div className="date">March 24th</div>
+               <div className="description">proceeds from this event benefit the individuals and families of Change, Inc.</div>
+               </div>
 
 
-       }
+               <div className="event3">
 
+               <div className="title">crab-a-palooza</div>
+               <div className="event3image"/>
+               <div className="date">September 15th</div>
+               <div className="description">All proceeds from this event benefit the many services we provide at Change, Inc.</div>
+               </div>
+
+       </div>
+    </div>
+            );
+           }
+        }
 
 
 class App extends Component {
