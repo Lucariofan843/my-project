3462fdc78f165645bbadf1e2defdf684923315e3
Lucariofan843 <Darklucario1553@gmail.com>
2017-07-28 11:20:30 -0400

    Part IV Commit (Added Columns)

--- src/App.js
+++ src/App.js
@@ -111,7 +111,8 @@ class Events extends Component {
            <div className="eventText">Change Inc's Events</div>
 
            <div className="eventsList">
-
+        <div className="columns is-gapless">
+            <div className="column">
                <div className="event1">
 
                        <div className="eventTitle">Golf Tournament</div>
@@ -119,7 +120,8 @@ class Events extends Component {
                        <div className="date">June 8th</div>
                        <div className="description">proceeds from this event benefit the individuals and families of Change, Inc.</div>
                </div>
-
+            </div>
+                <div className="column">
                <div className="event2">
 
                <div className="eventTitle">Bingo</div>
@@ -127,8 +129,9 @@ class Events extends Component {
                <div className="date">March 24th</div>
                <div className="description">proceeds from this event benefit the individuals and families of Change, Inc.</div>
                </div>
+                </div>
 
-
+            <div className="column">
                <div className="event3">
 
                <div className="eventTitle">crab-a-palooza</div>
@@ -136,7 +139,8 @@ class Events extends Component {
                <div className="date">September 15th</div>
                <div className="description">All proceeds from this event benefit the many services we provide at Change, Inc.</div>
                </div>
-
+        </div>
+        </div>
        </div>
 
     </div>
