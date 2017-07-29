8bb6bd5aaaaa117ddae8149b86cfcdf497d25b12
Lucariofan843 <Darklucario1553@gmail.com>
2017-07-29 19:40:57 -0400

    Part IV Commit (YouTube video embedded and box added)

--- src/App.js
+++ src/App.js
@@ -51,14 +51,20 @@ class Video extends Component {
     render() {
         return (
             <div className="box">
+                <div className="title">Very inspiring video!</div>
             <div className="youtubevideo">
 
+                <iframe
+                    title={`youtube_IueTjv1kfac`}
+                    src="https://www.youtube.com/embed/IueTjv1kfac"
+                    width={560}
+                    height={315}
+                    frameBorder={0}
+                    allowFullScreen
+                />
+
             </div>
-
-
             </div>
-
-
         );
     }
 }
