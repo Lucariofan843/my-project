52dbf3f63d9ea2da23597d6d9c6b3c59bad2fcee
Lucariofan843 <Darklucario1553@gmail.com>
2017-08-06 19:12:19 -0400

    Final Part Commit (Added GA)

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React,  { Component } from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import 'bulma/css/bulma.css';
+import ReactGA from 'react-ga';
 import About from './About';
 import Video from './Video';
 import Home from './Home';
 import Events from './Events';
 
+ReactGA.initialize('UA-104187477-1');
+
+
+/**
+ * @return {null}
+ */
+function Analytics(props){
+    ReactGA.set({ page: props.location.pathname + props.location.search });
+    ReactGA.pageview(props.location.pathname + props.location.search);
+    return null;
+}
 class App extends Component {
   render() {
     return (
       <Router>
           <section>
+         <Route path="/" component={Analytics}/>
         <header className="hero">
             <div className="hero-head">
                 <div className="header h1">
@@ -23,18 +36,15 @@ class App extends Component {
             <div className="nav">
               <div className="Link">
                   <h2 className="subtitle">
-
                 <nav>
                     <padding><Link to="/">Home</Link></padding> |
                     <padding><Link to="/About">About</Link></padding> |
                     <padding><Link to="/Events">Events</Link></padding> |
                     <padding><Link to="/Video">Video</Link></padding>
                 </nav>
-
                   </h2>
               </div>
             </div>
-
             <Route path="/" exact component={Home} />
             <Route path="/About" exact component={About} />
             <Route path="/Events" exact component={Events} />
@@ -48,7 +58,6 @@ class App extends Component {
           </section>
       </Router>
     );
-
   }
 }
 
