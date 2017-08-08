92cf052c74284eb8a99b1d1a9d865e3c099a2084
Lucariofan843 <Darklucario1553@gmail.com>
2017-08-08 11:30:55 -0400

    Final Commit Temp (Changes will be overviewed by creator)

--- src/Video.js
+++ src/Video.js
@@ -6,14 +6,14 @@ export default class Video extends Component {
             <div className="box is-block-mobile">
                 <div className="title">Very inspiring video!</div>
                 <div className="youtubevideo">
-
                     <iframe
                         title={`youtube_IueTjv1kfac`}
                         src="https://www.youtube.com/embed/IueTjv1kfac"
-                        width={448}
-                        height={252}
+                        width={368}
+                        height={207}
                         frameBorder={0}
                         allowFullScreen
+                        controls={2}
                     />
                 </div>
             </div>
